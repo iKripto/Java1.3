7bfb3cf5716b1315759f3256424e2fa2fb49de19
iKripto <yagosiqueira450@gmail.com>
2025-06-07 12:16:52 -0300

    Criacao do programa principal App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Fatorial fatorial = new Fatorial();
+
+        System.out.print("Digite um número inteiro nao negativo");
+        int numero = scanner.nextInt();
+
+        if (numero < 0) {
+            System.out.println("Numero negativo nao permitido");
+        } else {
+            long resultado = fatorial.calcular(numero);
+            System.out.println("Fatorial de " + numero + " e: " + resultado);
+        }
+
+        scanner.close();
     }
 }
